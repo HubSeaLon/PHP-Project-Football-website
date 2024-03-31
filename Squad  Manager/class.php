35d78847d22431fad40aee4199cf66a739c5ef10
HubSeaLon <152968357+HubSeaLon@users.noreply.github.com>
2024-04-01 01:46:13 +0200

    Add files via upload

--- Squad  Manager/class.php
+++ Squad  Manager/class.php
@@ -1,81 +1,64 @@
 <?php
 
-    // Ne fonctionne pas l'entrée des utilisateurs par la classe
+// -- Fonction inscription utilisateur -- //
 
-    // class newID {
-    //     private $login;
-    //     private $password;
-    //     private $verif;
+function nouveauID ($p_login, $p_password, $p_verif, $c){
+    
+    if(!empty($p_login) && !empty($p_password) && !empty($p_verif)) {
+        if($p_password == $p_verif){
+            $options = [
+                'cost' => 12, 
+            ];
 
-    //     function __construct($p_login, $p_password, $p_verif){
-    //         $this->login = $p_login;
-    //         $this->password = $p_password;
-    //         $this->verif = $p_verif;
-    //     }
-
-    //     public function nouveauId(){
-    //         require("connect.inc.php");
-    //         if(isset($_POST['formsend'])){
-    //             if(!empty($this->login) && !empty($this->password) && !empty($this->verif)) {
-    //                 if($this->password == $this->verif){
-    //                     $options = [
-    //                         'cost' => 12, 
-    //                     ];
-
-    //                     $hashpass = password_hash($this->password, PASSWORD_BCRYPT, $options);
+            $hashpass = password_hash($p_password, PASSWORD_BCRYPT, $options);
                         
-    //                     try {
-    //                         $q = $c->prepare("INSERT INTO entraineur (mail, mdp) VALUES (:mail, :mdp)");
-    //                         $q->execute([
-    //                             'mail' => $this->login,
-    //                             'mdp' => $hashpass
-    //                         ]);
-        
-    //                         echo 'Nouvel utilisateur enregistré';
-    //                     } catch (PDOException $e) {
-    //                         echo "Erreur lors de l'insertion dans la base de données : " . $e->getMessage();
-    //                     }
-                        
-    //                 } else echo "Les mots de passe ne correspondent pas";
-    //             }
-    //         }
-    //     }
-    // }
-
-    function nouveauID ($p_login, $p_password, $p_verif){
-
-        require("connect.inc.php");
-                if(!empty($p_login) && !empty($p_password) && !empty($p_verif)) {
-                    if($p_password == $p_verif){
-                        $options = [
-                            'cost' => 12, 
-                        ];
-
-                        $hashpass = password_hash($p_password, PASSWORD_BCRYPT, $options);
-                        
-                        try {
-                            $s = $c->prepare("SELECT mail FROM entraineur WHERE mail = :mail");
-                            $s->execute(['mail' => $p_login]);
+            try {
+                $s = $c->prepare("SELECT mail FROM entraineur WHERE mail = :mail");
+                $s->execute(['mail' => $p_login]);
                             
-                            $resultat = $s->rowCount();
+                $resultat = $s->rowCount();
 
-                            if($resultat == 0){
-                                $q = $c->prepare("INSERT INTO entraineur (mail, mdp) VALUES (:mail, :mdp)");
-                                $q->execute([
-                                    'mail' => $p_login,
-                                    'mdp' => $hashpass
-                                ]);
-                                echo 'Nouvel utilisateur enregistré';
-                                header("Location: controleur.php?page=login");
-                                exit;
-                            } else echo 'Un email existe déjà !';
-                            
-                        } catch (PDOException $e) {
-                            echo "Erreur lors de l'insertion dans la base de données : " . $e->getMessage();
-                        }
+                if($resultat == 0){
+                    $q = $c->prepare("INSERT INTO entraineur (mail, mdp) VALUES (:mail, :mdp)");
+                    $q->execute([
+                        'mail' => $p_login,
+                        'mdp' => $hashpass
+                    ]);
+                    
+                    header("Location: controleur.php?page=login");
+                    echo 'Nouvel utilisateur enregistré';
+                    exit;
+                } else echo 'Un email existe déjà !';
                         
-                    } else echo "Les mots de passe ne correspondent pas";
-                }
-            }
+            } catch (PDOException $e) {
+                echo "Erreur lors de l'insertion dans la base de données : " . $e->getMessage();
+            }                    
+        } else echo "Les mots de passe ne correspondent pas";
+    } else echo "Veuillez compléter les champs";
+}
+
+// -- Fonction connexion utilisateur -- //
+
+function connexionID($p_login, $p_password, $c){
+
+    if (!empty($p_login) && !empty($p_password)){
+        $q = $c->prepare("SELECT * FROM entraineur WHERE mail = :mail");
+        $q->execute(['mail' => $p_login]);
+        $result = $q->fetch();
+
+        if ($result == true){
+            $hashpassword = $result['mdp'];
+
+            if(password_verify($p_password, $hashpassword)){
+                echo "Mot de passe correct, connexion à votre compte";    
+                $_SESSION['login'] = $result['mail'];
+                header("Location: controleur.php?page=cindex");
+                exit;
+            } else echo "Mot de passe incorrect";
+
+        } else echo "L'email n'existe pas";
+
+    } else echo "Veuillez compléter l'ensemble des champs";
+}
 
 ?>
--- Squad  Manager/controleur.php
+++ Squad  Manager/controleur.php
@@ -7,42 +7,82 @@
     require("connect.inc.php");
 
     $tbs = new clsTinyButStrong;
-    $connexion ="";
+    $connexion = '';
 
-   
-    if (isset($_GET['page'])){
-        $page = $_GET['page'];
-        switch ($page){
-            case 'index':
-                $tbs->LoadTemplate('index.html');
-                break;
-            case 'services':
-                $tbs->LoadTemplate('Nos services.html');
-                break;
-            case 'qui-sommes-nous':
-                $tbs->LoadTemplate('Qui sommes-nous.html');
-                break;
-            case 'equipe':
-                $tbs->LoadTemplate('Notre equipe.html');
-                break;
-            case 'gererEquipe':
-                $tbs->LoadTemplate('gererEquipe.html');
-                break;
-            case 'login':
-                $tbs->LoadTemplate('login.html');
-                break;
-            case 'inscription':
-                if (isset($_POST["login"])) {                 
-                   nouveauID($_POST["login"], $_POST["passwd"], $_POST["verif"]);
-                }   
+    if(isset($_SESSION['login'])){
 
-                $tbs->LoadTemplate('inscription.html');
-                break;
-            default:
-                $tbs->LoadTemplate('404.html');   
-        } 
+        echo  "Connecté en tant que ". $_SESSION['login'];
+
+        if (isset($_GET['page'])){
+            $page = $_GET['page'];
+            switch ($page){
+                case 'cindex':
+                    $tbs->LoadTemplate('cindex.html');
+                    break;
+                case 'cservices':
+                    $tbs->LoadTemplate('cNos services.html');
+                    break;
+                case 'cqui-sommes-nous':
+                    $tbs->LoadTemplate('cQui sommes-nous.html');
+                    break;
+                case 'cequipe':
+                    $tbs->LoadTemplate('cNotre equipe.html');
+                    break;
+                case 'gererEquipe':
+                    $tbs->LoadTemplate('gererEquipe.html');
+                    break;   
+                case 'profil':
+                    $tbs->LoadTemplate('profil.html');
+                    break;
+                default:
+                    $tbs->LoadTemplate('cindex.html');   
+            } 
+        } else {
+            $tbs->LoadTemplate('cindex.html');
+        }
+
     } else {
-        $tbs->LoadTemplate('index.html');
+        echo "Non connecté";
+        if (isset($_GET['page'])){
+            $page = $_GET['page'];
+            switch ($page){
+                case 'index':
+                    $tbs->LoadTemplate('index.html');
+                    break;
+                case 'services':
+                    $tbs->LoadTemplate('Nos services.html');
+                    break;
+                case 'qui-sommes-nous':
+                    $tbs->LoadTemplate('Qui sommes-nous.html');
+                    break;
+                case 'equipe':
+                    $tbs->LoadTemplate('Notre equipe.html');
+                    break;
+                case 'gererEquipe':
+                    $tbs->LoadTemplate('gererEquipe.html');
+                    break;
+    
+                case 'login':
+                    if (isset($_POST["llogin"])){
+                        connexionID($_POST["llogin"], $_POST['lpasswd'], $c);
+                    }
+    
+                    $tbs->LoadTemplate('login.html');
+                    break;
+    
+                case 'inscription':
+                    if (isset($_POST["ilogin"])) {                 
+                       nouveauID($_POST["ilogin"], $_POST["ipasswd"], $_POST["verif"], $c);
+                    }   
+    
+                    $tbs->LoadTemplate('inscription.html');
+                    break;
+                default:
+                    $tbs->LoadTemplate('index.html');   
+            } 
+        } else {
+            $tbs->LoadTemplate('index.html');
+        }
     }
 
     $tbs->Show();
