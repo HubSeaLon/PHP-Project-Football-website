7cd9ddfc6ff3f6f7acf1a9182f6e27a6c9d1b6cc
HubSeaLon <152968357+HubSeaLon@users.noreply.github.com>
2024-04-08 23:42:07 +0200

    Add files via upload

--- Squad  Manager/js/code.js
+++ Squad  Manager/js/code.js
@@ -1,3 +1,5 @@
+// Partie creerEquipe.html
+
 function afficherOptions(){
     let pays = document.getElementById('selectPays').value;
     let options = '';
@@ -26,7 +28,9 @@ function afficherOptions(){
 
     document.getElementById("ligueOptions").innerHTML = options;
 }
+// Fin partie creerEquipe.html
 
+// Partie afficherEquipe.html
 function modifierEquipe(){
     let form = '';
     
@@ -65,3 +69,77 @@ function annulerSupp(){
     let form = '';
     document.getElementById('suppForm').innerHTML = form;
 }
+
+// Fin partie afficherEquipe
+
+
+// Partie gererJoueur.html
+document.addEventListener("DOMContentLoaded", function() {
+    gererJoueur();
+});
+
+// Fonction qui va générer boutons pour afficher les formulaires pour modifier, supprimer et ajouter des joueurs
+function gererJoueur(){ 
+    let sHTML = document.getElementById('joueurForm').innerHTML;
+
+    sHTML += "<button id='ajouterJoueur' name='ajouter' onclick='ajouterJoueur()'>Ajouter joueur</button>";
+    sHTML += "<button id='modifierJoueur' name='modifier' onclick='modifierJoueur()'>Modifier joueur</button>";
+    sHTML += "<button id='supprimerJoueur' name='supprimer' onclick='supprimerJoueur()'>Supprimer joueur</button>";
+
+    document.getElementById("joueurForm").innerHTML = sHTML;
+}
+
+function ajouterJoueur() {
+    let formAjouter = "";
+
+    formAjouter += "<h4>Ajouter un joueur</h4>";
+    formAjouter += "<form action='controleur.php?page=ajouterJoueur' method='post'> <fieldset>";
+    formAjouter += "<label for='numJoueur'> <b>Numéro</b>";
+    formAjouter += "<input type='text' id='numJoueur' name='numJoueur' placeholder='Numéro de votre joueur' required>";
+    formAjouter += "<label for='nomJoueur'> <b>Nom</b>";
+    formAjouter += "<input type='text' id='nomJoueur' name='nomJoueur' placeholder='Nom de votre joueur' required>";
+    formAjouter += "<label for='prenomJoueur'> <b>Prénom</b>";
+    formAjouter += "<input type='text' id='prenomJoueur' name='prenomJoueur' placeholder='Prénom de votre joueur' required>";
+    formAjouter += "<label for='nationaliteJoueur'> <b>Nationalité</b>";
+    formAjouter += "<input type='text' id='nationaliteJoueur' name='nationaliteJoueur' placeholder='Nationalité de votre joueur' required>";
+    formAjouter += "<label for='poste'> <b>Poste</b>";
+    formAjouter += "<select name='posteJoueur' id='poste-select'>";
+    formAjouter += "<option value='Attaquant'>Attaquant</option>";
+    formAjouter += "<option value='Milieu'>Milieu</option>";
+    formAjouter += "<option value='Défense'>Défense</option></select>";
+    formAjouter += "<input type='submit' id ='id_val' value='Valider'>";
+    formAjouter += "<input type='button' id ='id_val' value='Annuler'  onclick='annulerJoueur()'>";
+    formAjouter += "</fieldset> </form>";
+
+    document.getElementById("gererJoueurForm").innerHTML = formAjouter;
+
+
+
+    document.getElementById("id_num").addEventListener("input", function(event) {
+        var valeur = parseInt(event.target.value);
+        if (isNaN(valeur) || valeur < 1 || valeur > 100) {
+            event.target.setCustomValidity("Veuillez saisir un nombre entre 1 et 100.");
+        } else {
+            event.target.setCustomValidity("");
+        }
+    });
+}
+
+function modifierJoueur(){
+    formModifier = "";
+
+    formModifier += "<h4>Modifier un jouer</h4>";
+    formModifier += "<form action='controleur.php?page=gererJoueur' method='post'> <fieldset>";
+    formModifier += "<label for='modif'> <b>Numéro</b>";
+    
+}
+
+function supprimerJoueur(){
+    // ou sinon faire tout dans une fonction la section
+}
+
+function annulerJoueur(){
+    sHTML = '';
+    document.getElementById("gererJoueurForm").innerHTML = sHTML;
+}
+// Fin partie afficherEquipe*
